039d46d4c9ff8c6d2855e974ce98c54321ac85b1
juananother1 <72794428+juananother1@users.noreply.github.com>
2020-10-23 22:06:21 +0530

    Add files via upload

---
+++ AttackOnTitanCounter.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as bs
+
+print("Go to 'https://subslikescript.com/series/Attack_on_Titan-2560140' and select the episode to count")
+myUrl = input()
+
+client = ureq(myUrl)
+pageHTML = client.read()
+client.close()
+
+page_soup = bs(pageHTML, "html.parser")
+containers = page_soup.find("div", {"class":"full-script"})
+
+containers = containers.text
+
+
+print("In This episode, they say the word 'Titan' " +  (str)(count) + " times.")
